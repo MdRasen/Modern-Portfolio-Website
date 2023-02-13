e0ff3597c66d2d84d02b11344e5fb874c3a2e12a
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-13 22:36:35 +0600

    Text typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web", "Web Designer","Web Developer","Graphic Designer","Content Creator"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
