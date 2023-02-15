d6b7246ab0eee1bd67638c5d19ff8d72c86871cb
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-15 21:41:46 +0600

    Navigation menu animation updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
 // Typing animation
-var typed = new Typed(".typing",{
-    strings:["","Web", "Web Designer","Web Developer","Graphic Designer","Content Creator"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "Content Creator",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+// Aside
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let k = 0; k < totalSection; k++) {
+      allSection[k].classList.remove("back-section");
+    }
+    //Loop for removing active class
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    //Adding active class
+    this.classList.add("active");
+    showSection(this); //Function call
+  });
+}
+function showSection(element) {
+  //Loop for removing active class
+  for (let k = 0; k < totalSection; k++) {
+    allSection[k].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
